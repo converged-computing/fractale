beeef214d34be4866c91ae3a6ac6f6296830da05
vsoch <vsoch@users.noreply.github.com>
2026-02-27 16:36:30 -0800

    feat: add back agent command
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- fractale/cli/agent.py
+++ fractale/cli/agent.py
@@ -1,4 +1,6 @@
+from fractale.core.plan import Plan
 from fractale.engines import get_engine
+from fractale.logger.logger import logger
 
 from .runner import run_fractale
 
@@ -10,8 +12,19 @@ def main(args, extra, **kwargs):
     # Instantiate the Engine (native state machine)
     engine = get_engine(
         engine=args.engine,
-        plan=args.plan,
         backend=args.backend,
         max_attempts=args.max_attempts,
     )
+    valid_names = set([x.name for x in engine.get_local_tools()])
+    if args.agent not in valid_names:
+        logger.exit(f"{args.agent} is not a known sub-agent.")
+
+    prompt = " ".join(args.instruction)
+    plan = Plan(
+        {
+            "name": "Agentic plan",
+            "steps": [{"name": args.agent, "type": "plan", "inputs": {"goal": prompt}}],
+        }
+    )
+    engine.plan = plan
     run_fractale(engine, args)
